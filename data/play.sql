d4395e713baa85ab8a806ccb1112d2f02988b2ac
gichul.hong <mdipok@gmail.com>
2026-04-01 15:54:54 +0900

    doing no2

--- data/play.sql
+++ data/play.sql
@@ -23,8 +23,9 @@ SELECT  COUNT(*)
 SELECT  a.user
       , COUNT(a.rating)
   FROM  ratings a
+ WHERE  rating IS NOT NULL
  GROUP  BY a.user
- ORDER  BY 1 
+ ORDER  BY 2 DESC
 ;
 
 SELECT  a.item
@@ -72,4 +73,56 @@ SELECT  a.item
 
 
 
--- #2
+-- #2 데이터 보기
+SELECT  user
+      , MIN(rating) AS min_rating
+      , MAX(rating) AS max_rating
+      , COUNT(rating) AS cnt
+  FROM  ratings
+ WHERE  rating IS NOT NULL
+ GROUP  BY user
+;
+
+DROP TABLE if EXISTS prob_2_1;
+CREATE TABLE prob_2_1 AS
+SELECT  user
+      , CASE WHEN COUNT(rating) = 1 THEN 0 ELSE MIN(rating) END AS min_rating
+      , MAX(rating) AS max_rating
+      , COUNT(rating) AS cnt
+  FROM  ratings
+ WHERE  rating IS NOT NULL
+ GROUP  BY user;
+ 
+-- COMMIT;
+DROP TABLE if EXISTS prob_2_2;
+CREATE TABLE prob_2_2 AS
+SELECT  a.user AS user
+      , a.item AS item
+      , ROUND((a.rating - b.min_rating) / (b.max_rating - b.min_rating), 4) AS adj_rating 
+  FROM  ratings a
+  JOIN  prob_2_1 b
+    ON  a.user = b.user
+ WHERE  a.rating IS NOT NULL;
+ 
+COMMIT;
+
+
+-- 보정된 점수를 기반으로 아이템 별 사용자가 부여한 평점의 평균 구하기
+SELECT  item
+      , ROUND(AVG(adj_rating), 4) AS avg_rating
+  FROM  prob_2_2
+ GROUP  BY item
+ ;
+ 
+        SELECT  a.item AS item
+              , COUNT(a.rating) AS prediction
+          FROM  prob_2_2 a
+         WHERE  1 = 1
+           AND  a.item NOT IN ( SELECT  item
+                                  FROM  ratings
+                                 WHERE  user = {user}
+                                   AND  rating IS NOT NULL ) 
+           AND  a.rating IS NOT NULL
+         GROUP  BY a.item
+         ORDER  BY prediction DESC, a.item ASC
+         LIMIT  {rec_num}
